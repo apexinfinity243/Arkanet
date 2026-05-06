ed1506b6eea2bb8b5f7e03083f950f33b38898ea
私•Λρεχ乂Ιηfιηιτy•ㇱ <apexinfinity243@gmail.com>
2026-05-06 13:04:36 +0100

    Delete home.js

--- home.js
+++
@@ -1,8 +0,0 @@
-// home.js - navigation vers login ou signup
-document.getElementById("loginBtn").addEventListener("click", () => {
-  window.location.href = "login.html"; // page connexion
-});
-
-document.getElementById("signupBtn").addEventListener("click", () => {
-  window.location.href = "regist.html"; // page inscription
-});
